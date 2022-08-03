6513c276278548a4a927b37fe9fbac5369598980
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-02 22:13:00 -0700

    updating ./terraform/tf-module-vpc

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR
+  cidr_block           = var.VPC_CIDR
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     Name = "${var.PROJECT}-${var.ENV}"
   }
